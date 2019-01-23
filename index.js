c340faef9026fd6e7595d7237cf391131c196116
Denys Siroshtan <denis.siroshtan@mgid.com>
2019-01-23 16:22:13 +0200

    safari push

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 (function () {
-    console.log('start permission request...');
+    console.log('start information permission...');
     var result = window.safari.pushNotification.permission(
         'web.denyhf.github.io'
     );
-    
-    console.log(result);
+    console.log('current information',result);
+    console.log('request push permission');
+    Notification.requestPermission(function (e) {
+       console.log(e);
+    });
 })();
