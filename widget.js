b7902565a56f681237b6ba9f058bc9accf84edf8
Denys Siroshtan <denis.siroshtan@mgid.com>
2019-02-06 15:15:17 +0200

    add widget

--- widget.js
+++ widget.js
@@ -2,44 +2,13 @@ setInterval(function(){
   var informer = document.getElementById('M250178ScriptRootC649200');
   if (informer !== null) {
   var childs = informer.childNodes;
-  childs.forEach(function(e){if(e.localName == "frame" || e.localName == "script")
-       {
-         document.getElementById('M250178ScriptRootC649200').removeChild(e)
-       }
-     }
-   )
-     var D=new Date(),
-     d=document,b='body',
-     ce='createElement',
-     ac='appendChild',
-     st='style',
-     ds='display',
-     n='none',
-     gi='getElementById',
-     lp=d.location.protocol,
-     wp=lp.indexOf('http')==0?lp:'https:';
-     var i=d[ce]('iframe');
-     i[st][ds]=n;d[gi]("M250178ScriptRootC649200")[ac](i);
-     try{
-       var iw=i.contentWindow.document;
-       iw.open();
-       iw.writeln("<ht"+"ml><bo"+"dy></bo"+"dy></ht"+"ml>");
-       iw.close();
-       var c=iw[b];
-     } catch(e){
-       var iw=d;
-       var c=d[gi]("M250178ScriptRootC649200");
-     }
-     var dv=iw[ce]('div');
-     dv.id="MG_ID";
-     dv[st][ds]=n;
-     dv.innerHTML=649200;
-     c[ac](dv);
-     var s=iw[ce]('script');
-     s.async='async';
-     s.defer='defer';
-     s.charset='utf-8';
-     s.src=wp+"//jsc.idealmedia.io/r/a/racurs.ua.649200.js?t="+D.getYear()+D.getMonth()+D.getUTCDate()+D.getUTCHours();
-     c[ac](s);
-  }
-}, 0);
+  var code = '';
+  childs.forEach(function(e){
+    if (e.localName == "script") {
+      code = e.text
+      document.getElementById('M250178ScriptRootC649200').removeChild(e);
+      }
+    });
+   eval(code);
+ }
+}, 500);
